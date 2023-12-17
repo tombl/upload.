5993bb12ce1477457d56650093c27aec157d2cd4
tombl <git@tombl.dev>
2023-12-18 02:19:15 +0800

    fix eta bug caused by svelte update

--- src/UploadProgress.svelte
+++ src/UploadProgress.svelte
@@ -2,39 +2,59 @@
   import prettyBytes from "pretty-bytes";
   import prettyMilliseconds from "pretty-ms";
   import makeEta from "simple-eta";
+  import { onMount } from "svelte";
   import Spinner from "./Spinner.svelte";
 
-  let data = $state<{
-    eta: ReturnType<typeof makeEta>;
-    sent: number;
-    total: number;
-    estimate?: number;
-  }>();
+  let sent = $state(0);
+  let total = $state<number>();
+  let rate = $state(0);
 
+  let remaining: number;
+  let debouncedRemaining = $state<number>();
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      if (remaining === undefined) return;
+      if (
+        debouncedRemaining === undefined ||
+        Math.abs(debouncedRemaining - remaining) / remaining > 0.1
+      ) {
+        debouncedRemaining = remaining;
+        return;
+      }
+      if (debouncedRemaining > 1) debouncedRemaining--;
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  let eta: ReturnType<typeof makeEta>;
   export function report(e: ProgressEvent) {
-    data ??= {
-      eta: makeEta({ min: 0, max: e.total }),
-      sent: 0,
-      total: e.total,
-    };
-    data.eta.report(e.loaded);
-    data.sent = e.loaded;
-    const estimate = data.eta.estimate();
-    if (Number.isFinite(estimate)) {
-      data.estimate = Math.round(estimate) * 1000;
-    }
+    eta ??= makeEta({ min: 0, max: e.total });
+    eta.report(e.loaded);
+
+    sent = e.loaded;
+    total = e.total;
+
+    rate = eta.rate();
+
+    const estimate = eta.estimate();
+    if (Number.isFinite(estimate)) remaining = estimate;
   }
 </script>
 
-{#if data}
-  <h2>{((data.sent / data.total) * 100).toFixed(1)}%</h2>
-  <p>{prettyBytes(data.sent)} / {prettyBytes(data.total)}</p>
-  <p>{prettyBytes(data.eta.rate())}/s</p>
+{#if total === undefined}
+  <Spinner />
+{:else}
+  <h2>{((sent / total) * 100).toFixed(1)}%</h2>
+  <p>{prettyBytes(sent)} / {prettyBytes(total)}</p>
+  <p>{prettyBytes(rate)}/s</p>
   <p>
-    {#if data.estimate !== undefined}
-      {prettyMilliseconds(data.estimate, { colonNotation: true })}
+    {#if debouncedRemaining !== undefined}
+      {prettyMilliseconds(Math.round(debouncedRemaining) * 1000, {
+        colonNotation: true,
+      })}
+    {:else}
+      <span style:opacity={0.5}>0:00</span>
     {/if}
   </p>
-{:else}
-  <Spinner />
 {/if}
