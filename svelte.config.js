f4a57092780cf5569bd8aa5131523d00559ea840
Thomas Stokes <git@tombl.dev>
2024-12-22 18:20:04 +0800

    undo single bundle
    this put the signing code and the uploading code in the same bundle,
    massively bloating the download for the vast majority of people.

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,5 @@ import adapter from "@sveltejs/adapter-static";
 export default /** @satisfies {import('@sveltejs/kit').Config} */ ({
   kit: {
     adapter: adapter({ fallback: "404.html" }),
-    output: { bundleStrategy: "single" },
   },
 });
